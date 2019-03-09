85971b00a14d3069611e37ea73dc73a5414422b6
YernurOxikbayev <ernur.oksikbaev@gmail.com>
2019-03-09 16:03:50 +0700

    Добавьте файлы проекта.

---
+++ Dilate_Erode/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace lab4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button_open = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(165, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 512);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // button_open
+            // 
+            this.button_open.Location = new System.Drawing.Point(12, 24);
+            this.button_open.Name = "button_open";
+            this.button_open.Size = new System.Drawing.Size(141, 23);
+            this.button_open.TabIndex = 1;
+            this.button_open.Text = "Открыть изображение";
+            this.button_open.UseVisualStyleBackColor = true;
+            this.button_open.Click += new System.EventHandler(this.button_open_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(12, 53);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(141, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Сбросить";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.btn_Reset_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(12, 82);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(141, 23);
+            this.button3.TabIndex = 13;
+            this.button3.Text = "Дилатация";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.btn_Dilation_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(12, 111);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(141, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Эрозия";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.btn_Erosion_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(12, 140);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(141, 23);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Размыкание";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.btn_Opening_Click);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(12, 169);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(141, 23);
+            this.button1.TabIndex = 16;
+            this.button1.Text = "Замыкание";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btn_Closing_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 5);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(688, 544);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button_open);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Морфологическая обработка изображений";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button_open;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Dilate_Erode/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+using System.Runtime.InteropServices;
+
+
+
+namespace lab4
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap bmp;
+        private bool[] binIm;
+        private int maskSize = 3;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button_open_Click(object sender, EventArgs e)
+        {
+            getFile(pictureBox1);
+        }
+        public void getFile(PictureBox pictureBox1)
+        {
+
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files (*.BMP, *.JPG, *.GIF, *.TIF, *.PNG, *.ICO, *.EMF, *.WMF)|*.bmp;*.jpg;*.gif; *.tif; *.png; *.ico; *.emf; *.wmf";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                Image image = Image.FromFile(dialog.FileName);
+                int width = pictureBox1.Width;
+                int height = pictureBox1.Height;
+
+                bmp = new Bitmap(image, width, height);
+                ResetBinImage();
+                pictureBox1.Image = bmp;
+
+                button1.Enabled = true;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                button4.Enabled = true;
+                button5.Enabled = true;
+            }
+        }
+        private void btn_Reset_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = bmp;
+            ResetBinImage();
+            label1.Text = "";
+        }
+        private void ResetBinImage()
+        {
+            int width = bmp.Width;
+            int height = bmp.Height;
+
+            BitmapData data = bmp.LockBits(new Rectangle(0, 0, width, height), ImageLockMode.ReadOnly, PixelFormat.Format32bppArgb);
+
+            int bytes = data.Stride * data.Height;
+
+            byte[] pixelBuffer = new byte[bytes];
+            binIm = new bool[bytes/4];
+
+            IntPtr first_pixel = data.Scan0;
+
+            Marshal.Copy(first_pixel, pixelBuffer, 0, bytes);
+
+            bmp.UnlockBits(data);
+
+            float rgb = 0;
+            for (int i = 0; i < pixelBuffer.Length; i += 4)
+            {
+                rgb = pixelBuffer[i] * .21f;
+                rgb += pixelBuffer[i + 1] * .71f;
+                rgb += pixelBuffer[i + 2] * .071f;
+
+                binIm[i/4] = rgb > 127;
+            }
+        }
+
+        private Bitmap DilateAndErodeFilter(MorphologyType morphType, int matrixSize)
+        {//   Размеры изображения 
+            int width = bmp.Width;
+            int height = bmp.Height;
+            //Создание массивов байтов для хранения информации о пикселях изображения
+            bool[] resultBw = (bool[])binIm.Clone();
+            byte[] resultBuffer = new byte[width * height * 4];
+            //центральный пиксель смещен от границы ядра    
+            int filterOffset = (matrixSize - 1) / 2;
+            int calcOffset = 0;
+
+            int byteOffset = 0;
+
+            bool res;
+
+            if (morphType == MorphologyType.Dilation)
+            {
+                for (int offsetY = filterOffset; offsetY < height - filterOffset; offsetY++)
+                {
+                    for (int offsetX = filterOffset; offsetX < width - filterOffset; offsetX++)
+                    {  //положение пикселя центра ядра
+                        byteOffset = offsetY * width + offsetX;
+                        res = true;
+                        // Применение дилатации
+                        for (int filterY = -filterOffset;filterY <= filterOffset && res; filterY++)
+                        {
+                            for (int filterX = -filterOffset;filterX <= filterOffset; filterX++)
+                            {
+                                calcOffset = (offsetY + filterY) * width + offsetX + filterX;
+
+                                if (!binIm[calcOffset])
+                                {
+                                    res = false;
+                                    break;
+                                }
+                            }
+                        }
+
+                        resultBw[byteOffset] = res;
+                        //Запись обработанных данных во второй массив
+                        resultBuffer[byteOffset * 4] = res ? (byte)255 : (byte)0;
+                        resultBuffer[byteOffset * 4 + 1] = resultBuffer[byteOffset * 4];
+                        resultBuffer[byteOffset * 4 + 2] = resultBuffer[byteOffset * 4];
+                        resultBuffer[byteOffset * 4 + 3] = 255;
+                    }
+                }
+            }
+            else if (morphType == MorphologyType.Erosion)
+            {
+                for (int offsetY = filterOffset; offsetY < height - filterOffset; offsetY++)
+                {
+                    for (int offsetX = filterOffset; offsetX < width - filterOffset; offsetX++)
+                    {
+                        
+                        byteOffset = offsetY * width + offsetX;                                                                               // положение пикселя центра ядра
+                        res = false;
+                        // вычисления ядра
+                        for (int filterY = -filterOffset;filterY <= filterOffset && !res; filterY++)
+                        {
+                            for (int filterX = -filterOffset;filterX <= filterOffset; filterX++)
+                            {
+                                calcOffset = (offsetY + filterY) * width + offsetX + filterX;
+
+                                if (binIm[calcOffset])
+                                {
+                                    res = true;
+                                    break;
+                                }
+                            }
+                        }
+
+                        resultBw[byteOffset] = resultBw[byteOffset] ? resultBw[byteOffset] : res;
+                        //установим новые данные в другом массиве байтов 
+                        resultBuffer[byteOffset * 4] = resultBw[byteOffset] ? (byte)255 : (byte)0;
+                        resultBuffer[byteOffset * 4 + 1] = resultBuffer[byteOffset * 4];
+                        resultBuffer[byteOffset * 4 + 2] = resultBuffer[byteOffset * 4];
+                        resultBuffer[byteOffset * 4 + 3] = 255;
+                    }
+                }                
+            }            
+
+            binIm = resultBw;
+            // Создаём новое изображение, которое будет содержать обработанные данные
+            Bitmap resultBitmap = new Bitmap(width, height);
+            BitmapData resultData =resultBitmap.LockBits(new Rectangle(0, 0,resultBitmap.Width, resultBitmap.Height),ImageLockMode.WriteOnly,
+                       PixelFormat.Format32bppArgb);
+            // Копирование данных изображения в массивов байтов
+            Marshal.Copy(resultBuffer, 0, resultData.Scan0,resultBuffer.Length);
+            // Разблокируем бит из системной памяти так как у нас есть вся необходимая информация в массиве
+            resultBitmap.UnlockBits(resultData);
+
+            return resultBitmap;
+        }
+               
+
+        private void btn_Dilation_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = DilateAndErodeFilter(MorphologyType.Dilation, maskSize);
+            label1.Text = label1.Text + "Дилатация ";
+        }
+
+        private void btn_Erosion_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = DilateAndErodeFilter(MorphologyType.Erosion, maskSize);
+            label1.Text = label1.Text + "Эррозия ";
+        }
+
+        private void btn_Opening_Click(object sender, EventArgs e)
+        {
+            DilateAndErodeFilter(MorphologyType.Erosion, maskSize);
+            pictureBox1.Image = DilateAndErodeFilter(MorphologyType.Dilation, maskSize);
+            label1.Text = label1.Text + "Размыкание ";
+        }
+
+        private void btn_Closing_Click(object sender, EventArgs e)
+        {
+            DilateAndErodeFilter(MorphologyType.Dilation, maskSize);
+            pictureBox1.Image = DilateAndErodeFilter(MorphologyType.Erosion, maskSize);
+            label1.Text = label1.Text + "Замыкание ";
+        }
+
+        public enum MorphologyType
+        {
+            Dilation,
+            Erosion
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
